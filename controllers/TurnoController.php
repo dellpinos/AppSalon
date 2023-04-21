c4733da928da468aac8e1243a07bb147da0691e6
Martin del Pino <dellpinos7@gmail.com>
2023-04-21 12:34:30 -0300

    paginacion de turnos

--- controllers/TurnoController.php
+++ controllers/TurnoController.php
@@ -7,8 +7,10 @@ use MVC\Router;
 class TurnoController {
     public static function index(Router $router){
 
+//        session_start(); <<<<<<< Ya tengo iniciada la sesion, esto provoca un error Notice
+
         $router->render('turno/index', [
-            
+            'nombre' => $_SESSION['nombre']
         ]);
     }
 }
--- views/Turno/index.php
+++ views/Turno/index.php
@@ -1,2 +1,43 @@
 <h1 class="nombre-pagina">Pedir un turno nuevo</h1>
-<p class="descripcion-pagina">Elije tus servicios a continuación</p>
+
+<p class="descripcion-pagina">Elije tus servicios y coloca tus datos</p>
+
+<div id="app">
+    <nav class="tabs">
+        <button type="button" data-paso="1">Servicios</button>
+        <button type="button" data-paso="2">Información Turno</button>
+        <button type="button" data-paso="3">Resumen</button>
+    </nav>
+    <div class="seccion" id="paso-1">
+        <h2>Servicios</h2>
+        <p class="text-center" >Elige tus servicios a continuación</p>
+        <div id="servicios" class="listado-servicios"></div>
+    </div>
+    <div class="seccion" id="paso-2">
+        <h2>Tus Datos y Turno</h2>
+        <p class="text-center" >Coloca tus datos y fecha del turno</p>
+
+        <form class="formulario">
+            <div class="campo">
+                <label for="nombre">Nombre: </label>
+                <input type="text" id="nombre" placeholder="Tu nombre" value="<?php echo $nombre; ?>" disabled/>
+            </div>
+            <div class="campo">
+                <label for="fecha">Fecha: </label>
+                <input type="date" id="fecha"/>
+            </div>
+            <div class="campo">
+                <label for="hora">Hora: </label>
+                <input type="time" id="hora"/>
+            </div>
+        </form>
+    </div>
+    <div class="seccion" id="paso-3">
+        <h2>Resumen</h2>
+        <p class="text-center" >Verifica que la información sea correcta</p>
+    </div>
+    <div class="paginacion" >
+        <button id="anterior" class="boton">&laquo; Anterior</button>
+        <button id="siguiente" class="boton">Siguiente &raquo;</button>
+    </div>
+</div>
