0321b24aec4b8428ad7d374cf56a078ec0f8cb75
Martin del Pino <dellpinos7@gmail.com>
2023-04-24 19:08:39 -0300

    Modificando el objeto Turno en memoria

--- src/js/app.js
+++ src/js/app.js
@@ -2,23 +2,33 @@ let paso = 1; // le doy un valor inicial para que "mostrarSeccion" funcione la p
 const pasoInicial = 1;
 const pasoFinal = 3;
 
-document.addEventListener('DOMContentLoaded', function() {
+const turno = { // por default los objetos en js funcionan como "let" pueden ser modificados durante la ejecución
+    nombre: '',
+    fecha: '',
+    hora: '',
+    servicios: []
+}
+
+document.addEventListener('DOMContentLoaded', function () {
     iniciarApp();
 });
 
-function iniciarApp(){
+function iniciarApp() {
     mostrarSeccion(); // muestra y oculta las secciones
     tabs(); // Cambia la sección cuando cuando se presionan los tabs
     botonesPaginador(); // muestra y oculta botones paginador
     paginaSiguiente();
     paginaAnterior();
-    
+
     consultarAPI(); // Consulta la API en el Backend de PHP
+
+    nombreCliente(); // Almacena el nombre del cliente en el objeto Turno
+    seleccionarFecha(); // Almacena la fecha del turno en el objeto
 }
-function mostrarSeccion(){ // se ejecuta cada vez que hay un listener
+function mostrarSeccion() { // se ejecuta cada vez que hay un listener
     // Ocultar la seccion que tenga la clase mostrar
     const seccionAnterior = document.querySelector('.mostrar');
-    if(seccionAnterior){
+    if (seccionAnterior) {
         seccionAnterior.classList.remove('mostrar');
     }
 
@@ -29,7 +39,7 @@ function mostrarSeccion(){ // se ejecuta cada vez que hay un listener
 
     // Eliminar clase "actual" al tab anterior
     const tabAnterior = document.querySelector('.actual');
-    if(tabAnterior) {
+    if (tabAnterior) {
         tabAnterior.classList.remove('actual');
     }
 
@@ -38,11 +48,11 @@ function mostrarSeccion(){ // se ejecuta cada vez que hay un listener
     tab.classList.add('actual');
 }
 
-function tabs(){ // solo se ejecuta al iniciar la pagina
+function tabs() { // solo se ejecuta al iniciar la pagina
     const botones = document.querySelectorAll('.tabs button');
 
     botones.forEach(boton => {
-        boton.addEventListener('click', function(e){
+        boton.addEventListener('click', function (e) {
             paso = parseInt(e.target.dataset.paso)
 
             mostrarSeccion();
@@ -50,11 +60,11 @@ function tabs(){ // solo se ejecuta al iniciar la pagina
         });
     });
 }
-function botonesPaginador(){
+function botonesPaginador() {
     const paginaAnterior = document.querySelector('#anterior');
     const paginaSiguiente = document.querySelector('#siguiente');
 
-    if(paso === 1){
+    if (paso === 1) {
         paginaAnterior.classList.add('ocultar');
         paginaSiguiente.classList.remove('ocultar');
     } else if (paso === 3) {
@@ -66,37 +76,37 @@ function botonesPaginador(){
     }
     mostrarSeccion();
 }
-function paginaAnterior(){
+function paginaAnterior() {
     const paginaAnterior = document.querySelector('#anterior');
-    paginaAnterior.addEventListener('click', function(){
-        if(paso <= pasoInicial) return;
+    paginaAnterior.addEventListener('click', function () {
+        if (paso <= pasoInicial) return;
         paso--;
         botonesPaginador();
     });
 }
-function paginaSiguiente(){
+function paginaSiguiente() {
     const paginaSiguiente = document.querySelector('#siguiente');
-    paginaSiguiente.addEventListener('click', function(){
-        if(paso >= pasoFinal) return;
+    paginaSiguiente.addEventListener('click', function () {
+        if (paso >= pasoFinal) return;
         paso++;
         botonesPaginador();
-        
+
     });
 }
-async function consultarAPI(){
-    try{
+async function consultarAPI() {
+    try {
         const url = 'http://127.0.0.1:3000/api/servicios';
         const resultado = await fetch(url);
         const servicios = await resultado.json();
         mostrarServicios(servicios);
-    } catch(error){
+    } catch (error) {
         console.log(error);
     }
 }
-function mostrarServicios(servicios){
+function mostrarServicios(servicios) {
 
     servicios.forEach(servicio => {
-        const {id, nombre, precio} = servicio; // destruct, extrae el valor y crea la variable en una sola linea
+        const { id, nombre, precio } = servicio; // destruct, extrae el valor y crea la variable en una sola linea
 
         const nombreServicio = document.createElement('P');
         nombreServicio.classList.add('nombre-servicio');
@@ -109,15 +119,50 @@ function mostrarServicios(servicios){
         const servicioDiv = document.createElement('DIV');
         servicioDiv.classList.add('servicio');
         servicioDiv.dataset.idServicio = id; // el elemento, el metodo atributo, el nombre del atributo = asigno valor al atributo
+        servicioDiv.onclick = function(){
+            seleccionarServicio(servicio);
+        }
 
-       servicioDiv.appendChild(nombreServicio);
-       servicioDiv.appendChild(precioServicio);
+        servicioDiv.appendChild(nombreServicio);
+        servicioDiv.appendChild(precioServicio);
 
-       document.querySelector('#servicios').appendChild(servicioDiv);
+        document.querySelector('#servicios').appendChild(servicioDiv);
 
     });
 }
 
-// scripting es mas lento para el desarrollador pero tiene mejor performance y es mas seguro
-// scripting se refiere a crear html con código Js, conlleva muchas lineas de codigo a comparacion como lo haria con php/html
+function seleccionarServicio(servicio) {
+    const {id} = servicio;
+    const {servicios} = turno; // destructuring, extraigo el array "servicios" contenido dentro del objeto "turno" y creo la variable/objeto servicios
+    
+    // Identificar el elemento que recibe el click
+    const divServicio = document.querySelector(`[data-id-servicio= "${id}"]`);
+    // Comprobar si un servicio ya fue agregado o quitarlo
+    if(servicios.some(agregado => agregado.id === id)) {
+        // Eliminarlo
+        turno.servicios = servicios.filter( agregado => agregado.id !== id); // array method que crea un nuevo array con los elementos que cumplan cierta condicion
+        divServicio.classList.remove('seleccionado');
+    } else {
+        // Agregarlo
+        turno.servicios = [...servicios, servicio]; // spread operator hago una copia de "servicios" y le agrego "servicio", de esta forma si presiono en varios botones se van a ir sumando al objeto
+        divServicio.classList.add('seleccionado');
+    }
+    
+    console.log(turno);
+}
 
+function nombreCliente(){
+    const nombre = document.querySelector('#nombre').value;
+    turno.nombre = nombre;
+}
+function seleccionarFecha() {
+    const inputFecha = document.querySelector('#fecha');
+    inputFecha.addEventListener('input', function(e){
+        console.log(e.target.value);
+
+        const dia = new Date(e.target.value).getUTCDay();
+
+        if(dia )
+        console.log(dia);
+    });
+}
