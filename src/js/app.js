647b4a2321ec9a3ed66e414dba8656c13aa42a13
Martin del Pino <dellpinos7@gmail.com>
2023-04-24 09:30:58 -0300

    CSS a Servicios

--- src/js/app.js
+++ src/js/app.js
@@ -80,6 +80,7 @@ function paginaSiguiente(){
         if(paso >= pasoFinal) return;
         paso++;
         botonesPaginador();
+        
     });
 }
 async function consultarAPI(){
@@ -87,8 +88,36 @@ async function consultarAPI(){
         const url = 'http://127.0.0.1:3000/api/servicios';
         const resultado = await fetch(url);
         const servicios = await resultado.json();
-        console.log(servicios);
+        mostrarServicios(servicios);
     } catch(error){
         console.log(error);
     }
 }
+function mostrarServicios(servicios){
+
+    servicios.forEach(servicio => {
+        const {id, nombre, precio} = servicio; // destruct, extrae el valor y crea la variable en una sola linea
+
+        const nombreServicio = document.createElement('P');
+        nombreServicio.classList.add('nombre-servicio');
+        nombreServicio.textContent = nombre;
+
+        const precioServicio = document.createElement('P');
+        precioServicio.classList.add('precio-servicio');
+        precioServicio.textContent = `$${precio}`;
+
+        const servicioDiv = document.createElement('DIV');
+        servicioDiv.classList.add('servicio');
+        servicioDiv.dataset.idServicio = id; // el elemento, el metodo atributo, el nombre del atributo = asigno valor al atributo
+
+       servicioDiv.appendChild(nombreServicio);
+       servicioDiv.appendChild(precioServicio);
+
+       document.querySelector('#servicios').appendChild(servicioDiv);
+
+    });
+}
+
+// scripting es mas lento para el desarrollador pero tiene mejor performance y es mas seguro
+// scripting se refiere a crear html con código Js, conlleva muchas lineas de codigo a comparacion como lo haria con php/html
+
