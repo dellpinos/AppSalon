fd2c94cb18a08130f312e11eb6b3f07315110018
Martin del Pino <dellpinos7@gmail.com>
2023-04-30 21:39:35 -0300

    Admin vista

--- controllers/AdminController.php
+++ controllers/AdminController.php
@@ -3,16 +3,36 @@
 namespace Controllers;
 
 use MVC\Router;
+use Model\AdminTurno;
 
 
 class AdminController {
 
     public static function index(Router $router) {
-
         // session_start();
 
+
+        // Consultar DB
+        $query = "SELECT turnos.id, turnos.hora, CONCAT(usuarios.nombre, \" \" ,usuarios.apellido) AS cliente, ";
+        $query .= " usuarios.email, usuarios.telefono, servicios.nombre AS servicio, servicios.precio ";
+        $query .= " FROM turnos ";
+        $query .= " LEFT OUTER JOIN usuarios ";
+        $query .= " ON turnos.usuarioId=usuarios.id ";
+        $query .= " LEFT OUTER JOIN turnosServicios ";
+        $query .= " ON turnosServicios.turnoId=turnos.id ";
+        $query .= " LEFT OUTER JOIN servicios ";
+        $query .= " ON turnosServicios.servicioId=servicios.id ";
+//        $query .= " WHERE fecha = '{$fecha}' ;";
+
+
+        $turnos = AdminTurno::SQL($query);
+
+
         $router->render('admin/index', [
-            'nombre' => $_SESSION['nombre']
+            'nombre' => $_SESSION['nombre'],
+            'turnos' => $turnos
         ]);
     }
+
 }
+
--- views/admin/index.php
+++ views/admin/index.php
@@ -12,4 +12,31 @@
     </form>
 </div>
 
-<div id="turnos-admin"></div>
+<div id="turnos-admin">
+    <ul class="turnos">
+        <?php
+        $idTurno = '';
+        foreach ($turnos as $key => $turno) {
+            if ($idTurno !== $turno->id) {
+        ?>
+                <li>
+                    <p>ID: <span><?php echo $turno->id ?></span></p>
+                    <p>Hora: <span><?php echo $turno->hora ?></span></p>
+                    <p>Cliente: <span><?php echo $turno->cliente ?></span></p>
+                    <p>Email: <span><?php echo $turno->email ?></span></p>
+                    <p>Telefono: <span><?php echo $turno->telefono ?></span></p>
+
+                    <h3>Servicios</h3>
+
+                <?php $idTurno = $turno->id;
+            }  // Fin de if 
+                ?>
+                    <p class="servicio"><?php echo $turno->servicio . " " . $turno->precio;?></p>
+                <?php
+                $actual = $turno->id;
+                $proximo = $turnos[$key + 1]->id;    
+                ?>
+            <?php } // Fin de forEach 
+            ?>
+    </ul>
+</div>
