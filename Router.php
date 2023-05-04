dfb0b5125f0ad7250634bc9ffd3af110a4da273a
Martin del Pino <dellpinos7@gmail.com>
2023-05-04 17:51:26 -0300

    Cambios en el gitignore

--- Router.php
+++ Router.php
@@ -29,7 +29,7 @@ class Router
         // $auth = $_SESSION['login'] ?? null;
 
         // $currentUrl = $_SERVER['PATH_INFO'] ?? '/'; // <<<<<< solo para desarrollo
-        $currentUrl = $_SERVER['REQUEST_URI'] ?? '/';
+        $currentUrl = $_SERVER['REQUEST_URI'] === '' ? '/' : $_SERVER['REQUEST_URI'];
         $method = $_SERVER['REQUEST_METHOD']; 
         
 
